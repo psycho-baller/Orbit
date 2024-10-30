1f60c3206782a20bbddaeb6fb4d1539e767baf22
Rami Maalouf <rami.rami@ucalgary.ca>
2024-10-30 10:02:59 -0600

    adding in userLocationModel but not yet using it

--- Orbit/Models/UserLocationModel.swift
+++ Orbit/Models/UserLocationModel.swift
@@ -1,14 +1,12 @@
 //
-//  UserLocation.swift
+//  UserLocationModel.swift
 //  Orbit
 //
 //  Created by Rami Maalouf on 2024-10-29.
 //
 
-
-struct UserLocation: Codable, Identifiable {
-    let id: String                 // Unique user reference
-    let timestamp: Date            // Time of the location update
-    let latitude: Double           // User’s latitude in high-accuracy mode
-    let longitude: Double          // User’s longitude in high-accuracy mode
+struct UserLocationModel: Codable, Identifiable {
+    let id: String  // Unique user reference
+    let latitude: Double  // User’s latitude in high-accuracy mode
+    let longitude: Double  // User’s longitude in high-accuracy mode
 }
