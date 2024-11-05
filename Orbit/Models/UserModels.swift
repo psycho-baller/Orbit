5a8e556954e51ac8e3dd5835b48ba86cb06e5585
devont9122 <devon.tran@ucalgary.ca>
2024-11-04 20:40:36 -0700

    Update UserModels.swift
    it should be up to date to match what was in messages.backend now

--- Orbit/Models/UserModels.swift
+++ Orbit/Models/UserModels.swift
@@ -38,14 +38,33 @@ struct UserModel: Codable, Identifiable {
         
     }
         
-    //    var isOnline: Bool
-    //    var lastActive: Date
-    //    var lastActive:
-    //    let bio: String?
-    //    let friends: [String]?
-    //    let profilePictureId: String?  // Reference to the File ID
-    //    let settings: Settings?
-}
+    var profileImageURL: URL?
+       var currentAreaId: String?  // References Area collection
+       var inactiveAreas: [Int] = []  // Array of area_id references
+       //    var inactiveTimes: [TimeRangeConfig] = []
 
-typealias UserDocument = AppwriteModels.Document<UserModel>
+       //    init(accountId: String, name: String, interests: [String]? = nil, latitude: Double? = nil, longitude: Double? = nil, isInterestedToMeet: Bool? = nil) {
+       //            self.accountId = accountId
+       //            self.name = name
+       //            self.interests = interests
+       //            self.latitude = latitude
+       //            self.longitude = longitude
+       //            self.isInterestedToMeet = isInterestedToMeet
+       //        }
+       //
+       //    var isOnline: Bool
+       //    var lastActive: Date
+       //    var lastActive:
+       //    let bio: String?
+       //    let friends: [String]?
+       //    let profilePictureId: String?  // Reference to the File ID
+       //    let settings: Settings?
+   }
 
+   struct TimeRangeConfig: Codable {
+       var dayOfWeek: Int  // 0 is monday, 1 is tuesday, and so on
+       var startTime: Date
+       var endTime: Date
+   }
+
+   typealias UserDocument = AppwriteModels.Document<UserModel>
