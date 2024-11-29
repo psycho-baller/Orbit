07fb61e306421079cf790cd650102566f8c3a7a5
devont9122 <devon.tran@ucalgary.ca>
2024-11-28 18:44:00 -0700

    Will not display conversation as unread for the sender of the latest message

--- Orbit/Models/ConversationDetailModel.swift
+++ Orbit/Models/ConversationDetailModel.swift
@@ -13,10 +13,12 @@ struct ConversationDetailModel: Identifiable {
     var lastMessage: String
     var timestamp: String
     var isRead: Bool
+    var lastSenderId: String  //need this to check whether new message should be displayed or not (sender should not get new message dot for their own message)
     
     mutating func update(with message: MessageDocument) {
         self.lastMessage = message.data.message
         self.timestamp = message.createdAt
         self.isRead = message.data.isRead ?? false
+        self.lastSenderId = message.data.senderAccountId
     }
 }
