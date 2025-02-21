34c847460acc1ed388318da3caa9c7004a3d98b2
Rami Maalouf <rami.rami@ucalgary.ca>
2025-02-21 10:00:24 -0700

    added 3 new models to match the database (mockable🥳)

---
+++ Orbit/Models/MeetupApprovalModel.swift
@@ -0,0 +1,52 @@
+//
+//  UserModel.swift
+//  Orbit
+//
+//  Created by Rami Maalouf on 2025-02-20.
+//
+
+import Appwrite
+import CoreLocation
+import Foundation
+import UIKit
+
+struct MeetupApprovalModel: Codable, Equatable, CodableDictionaryConvertible {
+    let approvedBy: UserModel
+    let meetupRequest: MeetupRequestModel
+    let firstMessage: String
+
+    static func mock() -> Self {
+        return .init(
+            approvedBy: UserModel.mock(),
+            meetupRequest: MeetupRequestModel.mock(),
+            firstMessage: "Looking forward to this!"
+        )
+    }
+    // var id: String {
+    //     return "approvedB + meetupReques"
+    // }
+    //    init(
+    //    ) {
+    //        self.
+    //    }
+
+    //    func update(
+    //        name: String? = nil,
+    //    ) -> MeetupApprovalModel {
+    //        return MeetupApprovalModel(
+
+    //        )
+    //    }
+}
+
+typealias MeetupApprovalDocument = AppwriteModels.Document<MeetupApprovalModel>
+
+extension MeetupApprovalDocument {
+    static func mock() -> MeetupApprovalDocument {
+        return AppwriteModels.Document<MeetupApprovalModel>.mock(
+            data: MeetupApprovalModel.mock(),
+            collectionId: "meetup-approvals",
+            databaseId: "main-database"
+        )
+    }
+}
