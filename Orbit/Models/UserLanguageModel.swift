079955b3c5005e40b3575fdd1224b03558f07540
Rami Maalouf <rami.rami@ucalgary.ca>
2025-02-25 11:23:09 -0700

    Added a language struct that would be used by the JSON data.

--- Orbit/Models/UserLanguageModel.swift
+++ Orbit/Models/UserLanguageModel.swift
@@ -8,13 +8,24 @@
 import Appwrite
 import Foundation
 
-struct UserLanguageModel: Codable, Equatable, Identifiable, CodableDictionaryConvertible {
+struct UserLanguageModel: Codable, Equatable, Identifiable,
+    CodableDictionaryConvertible
+{
     let languageId: String
     let proficiency: Int  // 0-5 scale
 
     static func mock() -> Self {
         return .init(languageId: "ara", proficiency: 4)
     }
+
+    var id: String { languageId }
+}
+
+/// The model you want to decode from JSON
+struct Language: Codable, Identifiable {
+    let languageId: String
+    let name: String
+    let autonym: String
 
     var id: String { languageId }
 }
