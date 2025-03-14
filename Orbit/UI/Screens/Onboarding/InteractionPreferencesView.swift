d9758b421f0cb0953b493ce29a49e29a0d347824
Rami Maalouf <rami.rami@ucalgary.ca>
2025-03-14 11:18:18 -0600

    fix bug

--- Orbit/UI/Screens/Onboarding/InteractionPreferencesView.swift
+++ Orbit/UI/Screens/Onboarding/InteractionPreferencesView.swift
@@ -108,11 +108,11 @@ struct InteractionPreferencesView: View {
             question.options.contains { $0.isSelected }
         }
 
-        let hasValidAgeRange =
-            viewModel.preferredMinAge != nil && viewModel.preferredMaxAge != nil
-        let hasSelectedGenders = !viewModel.preferredGender.isEmpty
+//        let hasValidAgeRange =
+//            viewModel.preferredMinAge != nil && viewModel.preferredMaxAge != nil
+//        let hasSelectedGenders = !viewModel.preferredGender.isEmpty
 
-        return hasSelectedQuestions && hasValidAgeRange && hasSelectedGenders
+        return hasSelectedQuestions// && hasValidAgeRange && hasSelectedGenders
     }
 
     private func tagView(for option: QuestionOption, in question: Question)
